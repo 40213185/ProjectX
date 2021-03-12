770879f69bc82eaeb53d350f9285ab0692117562
kreed2401 <58709017+kreed2401@users.noreply.github.com>
2021-03-12 18:03:35 +0000

    Fixed Anchoring

---
+++ ProjectXUnityProject/Assets/Art&Sound/UI/Scripts/UIHandling.cs
@@ -0,0 +1,55 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIHandling : MonoBehaviour
+{
+    private Stats playerStats;
+    private Stats uiPlayerStats;
+    private GameObject[] apCollection;
+
+    public Slider hpSlider;
+    public Slider manaSlider;
+
+
+    public GameObject apGroup;
+    public GameObject apPoint;
+
+    private void Start()
+    {
+        playerStats = GameObject.FindGameObjectWithTag("Player").GetComponent<PlayerController>().GetStats();
+
+        hpSlider.maxValue = playerStats.GetMaxHealth();
+
+        hpSlider.value = playerStats.GetCurrentHealth();
+
+        apCollection = new GameObject[playerStats.GetMaxActionPoints()];
+        for (int i = 0; i < playerStats.GetMaxActionPoints(); i++)
+        {
+            apCollection[i] = Instantiate(apPoint, apGroup.transform);
+            Debug.Log(i);
+        }
+
+        
+
+        uiPlayerStats = playerStats;
+    }
+
+    public void UpdateUI() 
+    {
+        if (uiPlayerStats.GetCurrentHealth() != playerStats.GetCurrentHealth()) hpSlider.value = playerStats.GetCurrentHealth(); 
+        
+        if(uiPlayerStats.GetCurrentActionPoints() != playerStats.GetCurrentActionPoints()) 
+        {
+            foreach(GameObject e in apCollection) 
+            {
+                e.SetActive(false);
+            }
+            for(int i = 0; i < playerStats.GetCurrentActionPoints(); i++) 
+            {
+                apCollection[i].SetActive(true);
+            }
+        }
+    }
+}
